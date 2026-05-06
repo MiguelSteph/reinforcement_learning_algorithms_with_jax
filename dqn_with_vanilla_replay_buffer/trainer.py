cdd46b5a6b1f940f523a1bedc8d1f99f11a6ad14
KAKANAKOU Miguel <kakanakoumiguel@KAKANAKOUs-Laptop.local>
2026-05-06 23:01:27 +0200

    Update config and enable logging

--- dqn_with_vanilla_replay_buffer/trainer.py
+++ dqn_with_vanilla_replay_buffer/trainer.py
@@ -117,7 +117,7 @@ class AgentTrainer():
             self.rng_key, sample_rng_key = jax.random.split(self.rng_key, 2)
             sample_batch = self.buffer.sample(buffer_state, sample_rng_key, self.cfg.batch_size)
             new_dqn_state, metrics = self.train_step(dqn_state, sample_batch)
-            # self.train_summary_writer.add_scalar('loss', float(metrics['loss']), int(dqn_state.step))
+            self.train_summary_writer.add_scalar('loss', float(metrics['loss']), int(dqn_state.step))
 
         if self.t_target_sync == 0:
             new_target_params = jax.tree.map(lambda x: x.copy(), new_dqn_state.params)
