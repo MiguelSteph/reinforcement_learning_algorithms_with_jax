fd3b872ab7a4cf68d29616efbaa0132501ceb3ca
KAKANAKOU Miguel <kakanakoumiguel@KAKANAKOUs-Laptop.local>
2026-05-07 13:03:30 +0200

    Update env wrapper

--- dqn_with_vanilla_replay_buffer/env_wrapper.py
+++ dqn_with_vanilla_replay_buffer/env_wrapper.py
@@ -9,11 +9,11 @@ gym.register_envs(ale_py)
 class EnvWrapper:
     def __init__(self, env_id: str = "ALE/Breakout-v5", n_stack: int = 4):
         env = gym.make(env_id, frameskip=1)
-        env = AtariPreprocessing(env, grayscale_obs=False)
+        env = AtariPreprocessing(env, terminal_on_life_loss=True)
         self._env = FrameStackObservation(env, stack_size=n_stack)
 
     def _process_obs(self, obs) -> np.ndarray:
-        return np.array(obs).transpose(1, 2, 0)
+        return np.array(obs).transpose(1, 2, 0)  # (4,84,84) → (84,84,4)
 
     def reset(self, seed: int | None = None):
         obs, info = self._env.reset(seed=seed)
