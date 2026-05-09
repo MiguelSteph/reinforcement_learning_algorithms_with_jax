4b2e079b91057a30f22c94e224792f500cec5b2e
KAKANAKOU Miguel <kakanakoumiguel@KAKANAKOUs-Laptop.local>
2026-05-09 14:18:59 +0200

    Update model

--- dqn_with_vanilla_replay_buffer/model.py
+++ dqn_with_vanilla_replay_buffer/model.py
@@ -8,18 +8,18 @@ class QNetwork(nn.Module):
     @nn.compact
     def __call__(self, inputs: jnp.ndarray) -> jnp.ndarray:
         x = inputs.astype(jnp.float32) / 255.0
-        x = nn.Conv(features=16, kernel_size=(8, 8), strides=(4, 4), padding='VALID')(x)
+        x = nn.Conv(features=32, kernel_size=(8, 8), strides=(4, 4), padding='VALID')(x)
         x = nn.relu(x)
 
-        x = nn.Conv(features=32, kernel_size=(4, 4), strides=(2, 2), padding='VALID')(x)
+        x = nn.Conv(features=64, kernel_size=(4, 4), strides=(2, 2), padding='VALID')(x)
         x = nn.relu(x)
 
-        # x = nn.Conv(features=64, kernel_size=(3, 3), strides=(1, 1), padding='VALID')(x)
-        # x = nn.relu(x)
+        x = nn.Conv(features=64, kernel_size=(3, 3), strides=(1, 1), padding='VALID')(x)
+        x = nn.relu(x)
 
         x = x.reshape(x.shape[0], -1)
 
-        x = nn.Dense(256)(x)
+        x = nn.Dense(512)(x)
         x = nn.relu(x)
         x = nn.Dense(self.action_size)(x)
         return x
